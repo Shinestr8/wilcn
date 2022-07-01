19836fdada544beace1b023c340ebb2b89c5c476
Corentin <shinedownizstr8@gmail.com>
2022-07-01 13:26:20 +0200

    update url

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 async function loadNames() {
-    const response = await fetch('http://localhost:8080/lingo');
+    const response = await fetch('https://tm-stats-bknd.herokuapp.com/lingo');
     const lingo = await response.json();
     document.getElementById("name").textContent = lingo.name;
   }
