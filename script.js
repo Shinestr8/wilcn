4105a65e95027e2979fb42324d3cdca990794116
Shinestr8 <40238013+Shinestr8@users.noreply.github.com>
2022-12-10 14:40:47 +0100

    update url

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 async function loadNames() {
-    const response = await fetch('https://tm-stats-bknd.herokuapp.com/lingo');
+    const response = await fetch('https://polite-conkies-75f54b.netlify.app/.netlify/functions/api/lingo');
     const lingo = await response.json();
     document.getElementById("name").textContent = lingo.name;
   }
