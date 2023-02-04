4e8c70aa8e3da1a96807817f2321d3bc55a1d277
shinestr8 <shinedownizstr8@gmail.com>
2023-02-04 20:47:37 +0100

    getawaycar

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+const namediv = document.getElementById("name");
+
 async function loadNames() {
     const response = await fetch('https://polite-conkies-75f54b.netlify.app/.netlify/functions/api/lingo');
     const lingo = await response.json();
-    document.getElementById("name").textContent = lingo.name;
+    namediv.textContent = lingo.name;
   }
+
+namediv.addEventListener('click', async () => {
+  const response = await fetch('https://polite-conkies-75f54b.netlify.app/.netlify/functions/api/hobbit');
+  const hobbit = await response.json();
+  namediv.textContent = hobbit.name;
+})
 
   loadNames();
